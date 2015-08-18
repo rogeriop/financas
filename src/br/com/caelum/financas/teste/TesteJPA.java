f0adcb0a5011360c6056e4111c549f8af19ef660
rogeriop <pinhiroxrogerio@gmail.com>
2015-08-18 12:25:50 -0300

    fim 02 Configuracao e inicializacao do JPA

---
+++ src/br/com/caelum/financas/teste/TesteJPA.java
@@ -0,0 +1,33 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPA {
+
+	public static void main(String[] args) {
+		
+		double inicio = System.currentTimeMillis();
+		
+		Conta conta = new Conta();
+		conta.setTitular("Joao Ferreira");
+		conta.setBanco("HSBC");
+		conta.setNumero("12345");
+		conta.setAgencia("321");
+
+		EntityManager manager = new JPAUtil().getEntityManager();
+		
+		manager.getTransaction().begin();
+		manager.persist(conta);
+		manager.getTransaction().commit();
+
+		manager.close();
+		
+		double fim = System.currentTimeMillis();
+        System.out.println("Executado em: " + (fim - inicio)/1000 + "s");
+
+	}
+
+}
